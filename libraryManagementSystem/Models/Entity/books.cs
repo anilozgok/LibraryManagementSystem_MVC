681c7c602793c68bb9cb76012c553c4e8d3fc7d8
Anıl Can Özgök <71594949+anilozgok@users.noreply.github.com>
2022-08-09 15:16:16 +0300

    customDataAnnotation ve componentModel eklendi

--- libraryManagementSystem/Models/Entity/books.cs
+++ libraryManagementSystem/Models/Entity/books.cs
@@ -9,8 +9,11 @@
 
 namespace libraryManagementSystem.Models.Entity
 {
+    using libraryManagementSystem.CustomDataAnnotation;
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     
     public partial class books
     {
@@ -21,6 +24,8 @@ namespace libraryManagementSystem.Models.Entity
         }
     
         public int book_id { get; set; }
+
+
         public string book_name { get; set; }
         public int category { get; set; }
         public int author { get; set; }
